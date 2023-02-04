cdf030389293c18347cc88e68c218d319909903e
Jose Del Valle <jdelvalle88@live.com>
2023-02-04 16:01:28 -0500

    new codes and files added

---
+++ routes/notes.js
@@ -0,0 +1,65 @@
+// const notes = require('express').Router();
+// const uuid = require('./helpers/uuid');
+// const {
+//   readFromFile,
+//   readAndAppend,
+//   writeToFile,
+// } = require('./helpers/uuid');
+
+// // GET Route for retrieving all the tips
+// notes.get('/', (req, res) => {
+//   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+// });
+
+// // GET Route for a specific tip
+// notes.get('/:notes_id', (req, res) => {
+//   const noteId = req.params.note_id;
+//   readFromFile('./db/notes.json')
+//     .then((data) => JSON.parse(data))
+//     .then((json) => {
+//       const result = json.filter((note) => note.note_id === noteId);
+//       return result.length > 0
+//         ? res.json(result)
+//         : res.json('No tip with that ID');
+//     });
+// });
+
+// // DELETE Route for a specific tip
+// notes.delete('/:note_id', (req, res) => {
+//   const noteId = req.params.note_id;
+//   readFromFile('./db/notes.json')
+//     .then((data) => JSON.parse(data))
+//     .then((json) => {
+//       // Make a new array of all tips except the one with the ID provided in the URL
+//       const result = json.filter((note) => note.note_id !== noteId);
+
+//       // Save that array to the filesystem
+//       writeToFile('./db/notes.json', result);
+
+//       // Respond to the DELETE request
+//       res.json(`Item ${noteId} has been deleted 🗑️`);
+//     });
+// });
+
+// // POST Route for a new UX/UI tip
+// notes.post('/', (req, res) => {
+//   console.log(req.body);
+
+//   const { title, text, note } = req.body;
+
+//   if (req.body) {
+//     const newNote = {
+//       title,
+//       note,
+//       text,
+//       note_id: uuid(),
+//     };
+
+//     readAndAppend(newNote, './db/notes.json');
+//     res.json(`Note added successfully`);
+//   } else {
+//     res.error('Error in adding note');
+//   }
+// });
+
+// module.exports = notes;
--- server.js
+++ server.js
@@ -1,57 +1,47 @@
 const express = require('express');
 const path = require('path');
-// Helper method for generating unique ids
+const fs = require('fs');
+const noteData = require('./db/notes.json');
 const uuid = require('./helpers/uuid');
-const notes = require('./db/notes');
 const { clog } = require('./middleware/clog');
 
-const PORT = 3001;
+// const api = require('./routes/notes.js');
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use('/api', api);
+
 
 // Import custom middleware, "cLog"
 app.use(clog);
 
 app.use(express.static('public'));
-//GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
-// GET Route for notes page
-app.get('/notes', (req, res) =>
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(_dirname, '/public/index.html'))
+  );
+
+
+app.get('/api/db/notes.json', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+
 // GET request for notes
-app.get('/api/notes', (req, res) => {
-  console.info(`GET /api/notes`);
+app.get('/api/db/notes.json', (req, res) => {
+  console.info(`GET /api/db/notes.json`);
   res.status(200).json(notes);
 });
 
-// GET a single note
-app.get('/api/notes/:notes_id', (req, res) => {
-  if (req.params.notes_id) {
-    console.info(`${req.method} request received to get a single a note`);
-    const noteId = req.params.note_id;
-    for (let i = 0; i < notes.length; i++) {
-      const currentNotes = notes[i];
-      if (currentNotes.note_id === noteId) {
-        res.json(currentNotes);
-        return;
-      }
-    }
-    res.status(404).send('Note not found');
-  } else {
-    res.status(400).send('Notes ID not provided');
-  }
-});
+app.get('/api/db/notes', (req, res) => res.json(noteData));
 
-// POST request to add a notes
-app.post('/api/notes', (req, res) => {
+// POST request to add a note
+app.post('/api/db/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
 
@@ -59,10 +49,11 @@ app.post('/api/notes', (req, res) => {
   const { note } = req.body;
 
   // If all the required properties are present
-  if (note) {
+  if (note && title) {
     // Variable for the object we will save
     const newNote = {
-      note: Math.floor(Math.random() * 100),
+      note,
+      title,
       note_id: uuid(),
     };
 
@@ -78,39 +69,7 @@ app.post('/api/notes', (req, res) => {
   }
 });
 
-// // GET request for a specific review's upvotes
-// app.get('/api/upvotes/:review_id', (req, res) => {
-//   console.info(`${req.method} request received to get upvotes for a review`);
-//   for (let i = 0; i < reviews.length; i++) {
-//     const currentReview = reviews[i];
-//     if (currentReview.review_id === req.params.review_id) {
-//       res.status(200).json({
-//         message: `The review with ID ${currentReview.review_id} has ${currentReview.upvotes}`,
-//         upvotes: currentReview.upvotes,
-//       });
-//       return;
-//     }
-//   }
-//   res.status(404).json('Review ID not found');
-// });
-
-// // POST request to upvote a review
-// app.post('/api/upvotes/:review_id', (req, res) => {
-//   if (req.body && req.params.review_id) {
-//     console.info(`${req.method} request received to upvote a review`);
-//     const reviewId = req.params.review_id;
-//     for (let i = 0; i < reviews.length; i++) {
-//       const currentReview = reviews[i];
-//       if (currentReview.review_id === reviewId) {
-//         currentReview.upvotes += 1;
-//         res.status(200).json(`New upvote count is: ${currentReview.upvotes}!`);
-//         return;
-//       }
-//     }
-//     res.status(404).json('Review ID not found');
-//   }
-// });
-
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost: ${PORT}`)
 );
+
