65a7f501d957a1b23abdb26af8a03b5f6a150c6d
Jose Del Valle <jdelvalle88@live.com>
2023-02-04 20:45:00 -0500

    codes added onto files

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
+// Import express package
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// Require the JSON file and assign it to a const called `noteData`
 const noteData = require('./db/notes.json');
 const uuid = require('./helpers/uuid');
 const { clog } = require('./middleware/clog');
 
-// const api = require('./routes/notes.js');
 
 const PORT = process.env.PORT || 3001;
-
+// Initialize our app variable by setting it to the value of express()
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+
 
 
 // Import custom middleware, "cLog"
@@ -21,12 +22,12 @@ app.use(clog);
 
 app.use(express.static('public'));
 
-
+// GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(_dirname, '/public/index.html'))
   );
 
-
+// GET Route for notes page
 app.get('/api/db/notes.json', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
@@ -37,7 +38,7 @@ app.get('/api/db/notes.json', (req, res) => {
   console.info(`GET /api/db/notes.json`);
   res.status(200).json(notes);
 });
-
+// res.json() allows us to return JSON instead of a buffer, string, or static file
 app.get('/api/db/notes', (req, res) => res.json(noteData));
 
 // POST request to add a note
@@ -50,26 +51,56 @@ app.post('/api/db/notes', (req, res) => {
 
   // If all the required properties are present
   if (note && title) {
-    // Variable for the object we will save
+    // Const for the object we will save
     const newNote = {
       note,
       title,
       note_id: uuid(),
     };
 
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
+// Obtain existing notes
+fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
   } else {
-    res.status(500).json('Error in posting a note');
+    // Convert string into JSON object
+    const parsedNotes = JSON.parse(data);
+
+    // Add a new note
+    parsedNotes.push(newNote);
+
+    // Write updated notes back to the file
+    fs.writeFile(
+      './db/notes.json',
+      JSON.stringify(parsedNotes),
+      (writeErr) =>
+        writeErr
+          ? console.error(writeErr)
+          : console.info('Successfully updated notes!')
+    );
   }
 });
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost: ${PORT}`)
+const response = {
+  status: 'success',
+  body: newNote,
+};
+
+console.log(response);
+res.status(201).json(response);
+} else {
+  res.status(500).json('Error in posting note');
+}
+});
+// Wildcard route to direct users to a index.html page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+//Route to direct users to a notes.html page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
