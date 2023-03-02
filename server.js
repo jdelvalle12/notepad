31315bfa0af224372aad990b80fc5c2978f3090a
jdelvalle12 <104731082+jdelvalle12@users.noreply.github.com>
2023-03-01 22:07:10 -0500

    edits to server file

--- server.js
+++ server.js
@@ -23,43 +23,43 @@ app.use(clog);
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(_dirname, '/public/index.html'))
-  );
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(_dirname, '/public/index.html'))
+//   );
 
 // GET Route for notes page
-app.get('/api/db/notes.json', (req, res) =>
+app.get('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 
 // GET request for notes
-app.get('/api/db/notes.json', (req, res) => {
-  console.info(`GET /api/db/notes.json`);
+app.get('/api/notes', (req, res) => {
+  console.info(`GET /notes`);
   res.status(200).json(notes);
 });
 // res.json() allows us to return JSON instead of a buffer, string, or static file
-app.get('/api/db/notes', (req, res) => res.json(noteData));
+app.get('/api/notes', (req, res) => res.json(noteData));
 
 // POST request to add a note
-app.post('/api/db/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
 
   // Destructuring assignment for the items in req.body
-  const { note } = req.body;
+  const { title, text } = req.body;
 
   // If all the required properties are present
-  if (note && title) {
+  if (title && text) {
     // Const for the object we will save
     const newNote = {
-      note,
       title,
-      note_id: uuid(),
+      text,
+      id: uuid(),
     };
 
 // Obtain existing notes
-fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+fs.readFile('/api/notes', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
@@ -71,7 +71,7 @@ fs.readFile('./db/notes.json', 'utf8', (err, data) => {
 
     // Write updated notes back to the file
     fs.writeFile(
-      './db/notes.json',
+      '/api/notes',
       JSON.stringify(parsedNotes),
       (writeErr) =>
         writeErr
@@ -97,9 +97,9 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 //Route to direct users to a notes.html page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// app.get('/notes', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/notes.html'))
+// );
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
